0ee6f699d140cef82615f1fce6fe8124c4b32a21
Andrei Hava <andrei.hava@proton.me>
2024-04-06 01:38:29 +0300

    Add mayastor

--- render.sh
+++ render.sh
@@ -9,14 +9,29 @@ fi
 
 TYPE=${2:-worker}
 
-talosctl gen config \
-  --output rendered/$1.yaml \
-  --output-types $TYPE \
-  --with-cluster-discovery=false \
-  --with-secrets secrets.yaml \
-  --config-patch @patches/cluster-name.yaml \
-  --config-patch @nodes/$1.yaml \
-  --force \
-  omnicloud \
-  https://api.talos.omnilium.local:6443 \
-  "${@:3}"
+if [[ "$TYPE" == "worker" ]]; then
+  talosctl gen config \
+    --output rendered/$1.yaml \
+    --output-types $TYPE \
+    --with-cluster-discovery=false \
+    --with-secrets secrets.yaml \
+    --config-patch @patches/cluster-name.yaml \
+    --config-patch @patches/mayastor.yaml \
+    --config-patch @nodes/$1.yaml \
+    --force \
+    omnicloud \
+    https://api.talos.omnilium.local:6443 \
+    "${@:3}"
+else
+  talosctl gen config \
+    --output rendered/$1.yaml \
+    --output-types $TYPE \
+    --with-cluster-discovery=false \
+    --with-secrets secrets.yaml \
+    --config-patch @patches/cluster-name.yaml \
+    --config-patch @nodes/$1.yaml \
+    --force \
+    omnicloud \
+    https://api.talos.omnilium.local:6443 \
+    "${@:3}"
+fi
