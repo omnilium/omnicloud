d53de383856a67655efebbcb880c23b6f46ebb46
Andrei Hava <andrei.hava@proton.me>
2024-04-06 01:44:45 +0300

    Remove mayastor

--- render.sh
+++ render.sh
@@ -16,7 +16,6 @@ if [[ "$TYPE" == "worker" ]]; then
     --with-cluster-discovery=false \
     --with-secrets secrets.yaml \
     --config-patch @patches/cluster-name.yaml \
-    --config-patch @patches/mayastor.yaml \
     --config-patch @nodes/$1.yaml \
     --force \
     omnicloud \
