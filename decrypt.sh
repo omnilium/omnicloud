e12ef8e565b681e485b12e7e49c6a624f55b4e49
Andrei Hava <andrei.hava@proton.me>
2024-04-06 01:11:30 +0300

    Added issuers

--- decrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-sops -d -i secrets.yaml
-sops -d -i --input-type yaml --output-type yaml talosconfig
--- encrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-sops -e -i secrets.yaml
-sops -e -i --input-type yaml --output-type yaml talosconfig
