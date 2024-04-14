975ff618ce65f0a0997d0115801bab171806e5af
Andrei Hava <andrei.hava@proton.me>
2024-04-14 18:34:06 +0300

    Add Maxmind license

--- manager/main.py
+++ manager/main.py
@@ -20,6 +20,7 @@ sops_paths = [
     "manifests/cert-manager/omnicloud-ca.yaml",
     "manifests/flagsmith/cnpg-password.yaml",
     "manifests/flagsmith/release.yaml",
+    "manifests/nginx/release.yaml",
     "manifests/onedev/cnpg-password.yaml",
     "manifests/onedev/release.yaml",
     "manifests/roundcube/cnpg-password.yaml",
